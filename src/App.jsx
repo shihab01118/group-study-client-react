f2964d1bf580307b351a322e78bc567e76d2d9a2
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-05 02:03:46 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <MainLayout>
         <Outlet />
+        <Footer />
       </MainLayout>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,45 @@
-
-
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <footer className="bg-base-200 shadow">
+      <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto py-10">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <h2 className="text-center text-secondary text-3xl font-bold uppercase">
+          <span className="text-[#2E0EAE]">Edu</span>Care
+          </h2>
+          <ul className="flex justify-center flex-wrap items-center my-6 md:my-0 text-sm font-medium text-gray-500 sm:mb-0">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-    );
+        <hr className="my-6 border-gray-300 sm:mx-auto lg:my-8" />
+        <span className="block text-sm text-gray-500 text-center">
+          © 2023{" "}
+          <a href="#" className="hover:underline">
+            EDUCARE
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
