6491f69589c167781cf46c9f2278a6b619724cfe
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-05 17:24:08 +0600

    private route added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,11 +1,29 @@
+import PropTypes from "prop-types";
+import useAuth from "../hooks/useAuth";
+import { Navigate } from "react-router-dom";
+import loading_animation from '../assets/animations/loading_animation.json';
+import Lottie from "lottie-react";
 
-
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+    const {user, loading} = useAuth();
+    
+    if (loading) {
+        return (
+          <div className="h-screen flex justify-center items-center">
+            <Lottie animationData={loading_animation}></Lottie>
+          </div>
+        );
+      }
+    
+      if (user) {
+        return children;
+      }
+    
+      return <Navigate to="/login" state={location.pathname}></Navigate>;
 };
+
+PrivateRoute.propTypes = {
+    children: PropTypes.node,
+  };
 
 export default PrivateRoute;
