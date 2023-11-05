5b33de5ac52d2a56364c7020d2b283aefc0b1188
shihab01118 <shahed.shihab.0@gmail.com>
2023-11-06 05:14:55 +0600

    assignments filtering functionality by difficulty level added

--- src/components/AssignmentCard.jsx
+++ src/components/AssignmentCard.jsx
@@ -1,37 +1,40 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import Aos from 'aos';
-import "aos/dist/aos.css"
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const AssignmentCard = ({ assignment }) => {
   const { _id, title, mark, imgUrl, level } = assignment || {};
 
   useEffect(() => {
-    Aos.init({duration: 1500})
-  }, [])
+    Aos.init({ duration: 1500 });
+  }, []);
 
   return (
-    <div data-aos="fade-up" className="card bg-base-100 shadow-xl image-full">
+    <div data-aos="fade-up" className="card bg-base-100 rounded-lg shadow-xl">
       <figure>
-        <img
-          src={imgUrl}
-          alt={title}
-        />
+        <img src={imgUrl} alt={title} className="h-[200px] w-full" />
       </figure>
-      <div className="card-body flex justify-center items-center">
+      <div className="card-body">
         <div className="space-y-5">
-        <h2 className="card-title text-base-400">{title}</h2>
-        <div>
-        <p className="text-gray-300 mb-3">Difficulty: {level}</p>
-        <p className="text-gray-300">Total Marks: {mark}</p>
-        </div>
-        <div className="card-actions justify-end">
-          <Link to={`/assignment_details/${_id}`}>
-          <button className="btn bg-[#6440FA] rounded-lg capitalize font-semibold text-white border-none hover:text-black hover:bg-secondary btn-sm">View</button>
-          </Link>
-          <button className="btn bg-primary rounded-lg capitalize font-semibold text-white border-none hover:text-black hover:bg-secondary btn-sm">Update</button>
-        </div>
+          <h2 className="card-title text-[#1A064E]">{title}</h2>
+          <div>
+            <p className="text-gray-600 mb-3">Difficulty: {level}</p>
+            <p className="text-gray-600">Total Marks: {mark}</p>
+          </div>
+          <div className="card-actions justify-end">
+            <Link to={`/assignment_details/${_id}`}>
+              <button className="btn bg-[#6440FA] rounded-lg capitalize font-semibold text-white border-none hover:text-black hover:bg-secondary btn-sm">
+                View
+              </button>
+            </Link>
+            <Link to={`/upadate_assignment/${_id}`}>
+              <button className="btn bg-primary rounded-lg capitalize font-semibold text-white border-none hover:text-black hover:bg-secondary btn-sm">
+                Update
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import AssignmentCard from "../components/AssignmentCard";
 import Loading from "../components/Loading";
-import useGetAssignments from "../hooks/useGetAssignments";
+import { useQuery } from "@tanstack/react-query";
+// import useGetAssignments from "../hooks/useGetAssignments";
 
 const Assignments = () => {
-  const { assignments, isLoading } = useGetAssignments();
+  const [difficultyLevel, setDifficultyLevel] = useState("All");
+  console.log(difficultyLevel);
+
+  const {
+    data: assignments,
+    isLoading,
+    // refetch,
+  } = useQuery({
+    queryKey: ["assignments"] ,
+    queryFn: async () => {
+      const data = await fetch(
+        `http://localhost:5000/api/v1/user/assignments?difficulty=${difficultyLevel}`
+      );
+      return await data.json();
+    },
+  });
 
   if (isLoading === true) {
     return <Loading />;
   }
 
   return (
-    <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto my-16 md:my-20 lg:my-24">
+    <div className="max-w-6xl mx-8 md:mx-16 lg:mx-auto mt-10 mb-16 md:mb-20 lg:mb-24">
+      <div className="flex justify-between mb-6">
+        <h2 className="text-2xl md:text-4xl font-bold text-[#1A064E]">
+          Assignmmets
+        </h2>
+        <select
+          value={difficultyLevel}
+          onChange={(e) => setDifficultyLevel(e.target.value)}
+          className="select rounded-lg shadow-lg w-full max-w-[180px] bg-base-100 focus:outline-none text-gray-700 font-medium"
+        >
+          <option value="All">All</option>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
+        </select>
+      </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         {assignments?.map((assignment) => (
           <AssignmentCard key={assignment._id} assignment={assignment} />
